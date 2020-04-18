ec0c0fe4de8beca38d3d9c1382d232ade7d7d9a4
Przemek <przemek.bzowka@gmail.com>
2020-04-18 19:41:10 +0200

    start with mobile slider

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./styles/index.scss";
+import Swiper from "swiper";
+
+const mySwiper = new Swiper(".swiper-container", {
+  slidesPerView: 1.3,
+  spaceBetween: 30,
+  slidesPerColumnFill: "row",
+  direction: "horizontal",
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: true,
+  },
+});
